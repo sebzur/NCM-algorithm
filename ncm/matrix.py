0f0fca7d9722e0dea2dd67a71de2e20b22118abd
k.wojtiuk@codelab.pl <k.wojtiuk@codelab.pl>
2019-06-06 13:16:08 +0200

    dodanie możliwości liczenia z r podanym przez użytkownia

--- ncm/matrix.py
+++ ncm/matrix.py
@@ -34,46 +34,38 @@ class Matrix(object):
     def __init__(self):
         self.verbose = rank == 0
 
-    def __call__(self, signal, max_m, tau, f_min, f_max, r_steps, normalize=True, selfmatches=False):
+    def __call__(self, signal, max_m, tau, rmode, rdiff, normalize=True, selfmatches=False):
         if tau > 1:
             print('Sorry, at this moment the implementation only allows for tau=1. This will be changed soon. Stay tuned!')
             return
-        return self.get_matrix(signal, max_m, tau, f_min=f_min, f_max=f_max, r_steps=r_steps, normalize=normalize, selfmatches=selfmatches)
+        if rmode != 'steps' and  rmode != 'diff':
+            print('Sorry, You must choose the rmode: "diff" or "steps"!')
+            return
+
+        return self.get_matrix(signal, max_m, tau, rmode=rmode, rdiff=rdiff, normalize=normalize, selfmatches=selfmatches)
 
 
-    def get_treshold_range(self, signal, f_min, f_max, steps):
+    def get_treshold_range(self, signal, rmode, rdiff):
         """ Returns the treshold ranges.
 
         Having the signal provided, returns the set of 
         treshold (r-filters) values.
 
         """
-        r_std = signal.std()
-        #r_std = 1.0
-
-        r_min =  r_std  * f_min
-        r_max = r_std  * f_max
-        r_step = (r_max - r_min) / steps
-        if self.verbose:
-            print('The corsum matrix will be created for [%.2f, %.2f] tresholds range created with %d steps. SD=%.2f' % (r_min, r_max, steps, r_std))
-
-        #normalized_log_spaced = (numpy.logspace(0, 1) - 1)/10.0
-        #delta = r_max - r_min
-        #log_spaced = r_min + normalized_log_spaced * delta
-        #return log_spaced[::-1]
-
-        # the r_range array is reverted
+        r_min = 0
+        r_max = 1.3 * (float(max(signal))-float(min(signal)))
+        if rmode == 'steps':
+            r_step = (r_max-r_min) / rdiff
+        else:
+            r_step = rdiff
         return numpy.arange(r_min, r_max, r_step)[::-1]
 
-
-    def get_matrix(self, signal, max_m, tau, f_min, f_max, r_steps, normalize, selfmatches):
+    def get_matrix(self, signal, max_m, tau, rmode, rdiff, normalize, selfmatches):
         matrix = ncm.NCMatrix(signal)
-        r_range = self.get_treshold_range(signal, f_min, f_max, r_steps)
+        r_range = self.get_treshold_range(signal, rmode, rdiff)
         # now, crete m_range as: [1 .. max_m]
         # we increment the second `range` argument to get the max_m processed
         m_range = range(1, max_m + 1)
-        if self.verbose:
-            print('Preparing for correlation sums matrix calculation. The matrix dimension will be %d x %d.' % (r_steps, max_m))
         c_m = matrix.corsum_matrix(m_range, r_range, tau, normalize, selfmatches)
         if rank == 0:
             return self.merge_data(c_m, r_range)
